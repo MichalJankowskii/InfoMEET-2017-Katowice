032fa28a229c5bccce22fc0a013aec0baacbbbd5
Michał Jankowski <jankowski.m@gmail.com>
2017-05-15 20:38:41 +0200

    Evil code

---
+++ source/2. Evil code/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo2
+{
+    public class Program
+    {
+        // Example from https://github.com/jskeet/DemoCode/tree/master/Abusing%20CSharp/Code/WhatTheHeck
+
+        public static void Main(string[] args)
+        {
+            var limit = 10;
+            limit = "five";
+            for (var x = 0; x < limit; x++)
+            {
+                Console.WriteLine(x);
+                Console.WriteLine("The current value of x is: {0}", x);
+            }
+        }
+    }
+}
