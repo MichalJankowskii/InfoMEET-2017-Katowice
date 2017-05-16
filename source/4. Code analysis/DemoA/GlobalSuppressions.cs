062d44d4a1c1f3c75db7bfa9b4ecfd9c36b505f3
Michał Jankowski <jankowski.m@gmail.com>
2017-05-16 20:43:19 +0200

    Added name in suppression file

--- source/4. Code analysis/DemoA/GlobalSuppressions.cs
+++ source/4. Code analysis/DemoA/GlobalSuppressions.cs
@@ -2,5 +2,5 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Demo purpose")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Demo purpose")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "MJI: Demo purpose")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "MJI: Demo purpose")]
