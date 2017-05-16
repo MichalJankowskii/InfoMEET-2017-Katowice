e52f0781ea2f37cc05fd44c24b627c7de5ffa418
Michał Jankowski <jankowski.m@gmail.com>
2017-05-16 16:46:31 +0200

    Class with styleCop errors

---
+++ source/4. Code analysis/WithStyleCopErrors.cs
@@ -0,0 +1,30 @@
+﻿namespace Demo4
+{
+    public class WithStyleCopErrors
+    {
+
+
+        public int[] sort(int[] Array)
+        {
+            int temp;
+            for (int i = 0; i <= Array.Length - 2; i++) {
+
+
+
+                for (int j = 0; j <= Array.Length - 2; j++)
+                { if (Array[j] > Array[j + 1])
+                    {
+                        temp = Array[j + 1];
+                        Array[j + 1] = Array[j];
+                        Array[j] = temp;
+                    }
+                }
+  
+            }
+
+
+            return Array;
+
+        }
+    }
+}
